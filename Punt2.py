9ebdb047c12fd1f4258f9c90e8da649867ffd999
John Jaime <jhonjaimemadrid02@gmail.com>
2022-10-29 15:09:38 -0500

    Check 1

--- Punt2.py
+++ Punt2.py
@@ -6,49 +6,61 @@ v_mill = arc_ex['Venta Millones']
 p_col = arc_ex['Plataforma']
 a_col = arc_ex['Año']
 
+
+
+
 def v_tot ():
   v_tot=arc_ex['Venta Millones'].sum() #Toma todos los valores de la columna.
   return v_tot #Los suma y retorna el valor en la variable.
 
-def av_tot (A):
-  s_tot=0
-  for x in range(len(a_col)):
-    val = arc_ex.loc[x,'Año']
-    if A == val:
-        m = arc_ex.loc[x,'Venta Millones']
-        s_tot=s_tot+m
-  return s_tot
+def av_tot ():
+    df = arc_ex
+    df2 = df.groupby('Año').sum('Venta Millones')
+    print(df2)
 
 def v_plat (plat):
-  s_tot =0
-  for x in range (len(p_col)):
-    v_str = arc_ex.loc[x,'Plataforma']
-    if v_str == plat:
-      i_int=arc_ex.loc[x,'Venta Millones']
-      s_tot =s_tot+i_int
-      
-  return s_tot
 
-def ap_max (A):
-    ac_v=0
-    mx_v=0
-    pl1 = 0
-    for x in range(len(a_col)):
-        v_s = arc_ex.loc[x,'Año']
-        if A==v_s:
-            plat=arc_ex.loc[x,'Plataforma']
-            pl1 = plat
-            if A==v_s and pl1 == plat:
-              v_m=arc_ex.loc[x,'Venta Millones']
-              ac_v=ac_v+v_m
-    return ac_v
-    return pl1
-x = ap_max(2000)  
-print(f'{x}')
+  df =arc_ex[arc_ex['Plataforma']==plat][['Venta Millones','Plataforma']]
+  df2 = df.groupby('Plataforma').sum('Venta Millones')
+  print(df2)
 
+def ap_max (i):
+    
+    df = arc_ex[arc_ex['Año']==i][['Plataforma','Venta Millones']]
+    df2 = df.groupby('Plataforma').sum('Venta Millones')
+    max_df2 = df2['Venta Millones'].max()
+    print(df2[df2['Venta Millones']==max_df2])
 
+# i=int(input('Año: '))
+# ap_max(i)
 
-#i= str(input('Plataforma en mayusculas: '))
-#v_plat(i)
+def jc_max (j):
+  df = arc_ex[arc_ex['Nombre']==j][['Región','Venta Millones']]
+  df2 = df.groupby('Región').sum('Venta Millones')
+  print(df2)
+# j=str(input('Ingresar nombre del juego: '))
+# jc_max(j)
 
+def rj_max (r):
+  df = arc_ex[arc_ex['Región']==r][['Nombre','Venta Millones']]
+  df2 = df.groupby('Nombre').sum('Venta Millones')
+  max_df2 = df2['Venta Millones'].max()
+  print(df2[df2['Venta Millones']==max_df2])
 
+def cv_tot ():
+  df= arc_ex[['Región','Venta Millones']]
+  df2 = df.groupby('Región').sum('Venta Millones')
+  # print(df2)
+  region=['Europa','Japón','Norte America', 'Otros']
+  venta =[937.84,456.02,1708.37,322.19]
+  explode = (0.05, 0.05, 0.05, 0.05)
+
+  plt.pie(venta , labels=region, explode=explode)
+
+  centre_circle = plt.Circle((0, 0), 0.70, fc='white')
+  fig = plt.gcf()
+
+  fig.gca().add_artist(centre_circle)
+
+  plt.show
+cv_tot()
